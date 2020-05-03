3a0cea6bc6f203978abd7d06ad56d46fba8cfc07
Michael Yaworski <mikeyaworski@hotmail.com>
2020-05-03 07:37:26 -0400

    Added clicking on question actions Version bump to 1.0.2

--- src/content-script.js
+++ src/content-script.js
@@ -3,6 +3,16 @@ function claim() {
   if (claimQuestBtn) claimQuestBtn.click();
 }
 
+function clickQuestActions() {
+  // all the quest actions which have not been done yet
+  const claimQuestActions = [...document.querySelectorAll('.quest-action-container')].filter(claimQuestAction => claimQuestAction.querySelector('i.icon-circle-empty'));
+
+  claimQuestActions.forEach(claimQuestAction => {
+    const actionLink = claimQuestAction.querySelector('a');
+    if (actionLink) actionLink.click();
+  });
+}
+
 function closeModal() {
   const closeModalBtn = document.querySelector('.icon-cancel-thin');
   if (closeModalBtn) closeModalBtn.click();
@@ -16,6 +26,7 @@ function openQuestsTab() {
 const INTERVALS = Object.freeze({
   questsTab: 60 * 1000,
   claim: 5 * 1000,
+  clickQuestActions: 5 * 1000,
   closeModal: 5 * 1000,
 });
 
@@ -38,4 +49,5 @@ chrome.storage.onChanged.addListener((changes) => {
 });
 
 setInterval(claim, INTERVALS.claim);
+setInterval(clickQuestActions, INTERVALS.clickQuestActions);
 setInterval(closeModal, INTERVALS.closeModal);
