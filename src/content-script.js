0b82bcada90e91e87e9962c9536e0acb7e491138
Michael Yaworski <mikeyaworski@hotmail.com>
2020-05-08 14:42:13 -0400

    Added support for voting and retweeting

--- src/content-script.js
+++ src/content-script.js
@@ -1,6 +1,10 @@
+function click(element) {
+  if (element) element.click();
+}
+
 function claim() {
   const claimQuestBtn = document.querySelector('.styles__ClaimReward-ga76s6-0');
-  if (claimQuestBtn) claimQuestBtn.click();
+  click(claimQuestBtn);
 }
 
 function clickQuestActions() {
@@ -9,32 +13,67 @@ function clickQuestActions() {
 
   claimQuestActions.forEach(claimQuestAction => {
     const actionLink = claimQuestAction.querySelector('a');
-    if (actionLink) actionLink.click();
+    click(actionLink);
   });
 }
 
 function closeModal() {
   const closeModalBtn = document.querySelector('.icon-cancel-thin');
-  if (closeModalBtn) closeModalBtn.click();
+  click(closeModalBtn);
 }
 
 function openQuestsTab() {
-  const questsTabBtn = document.querySelector('button[title=Quests]');
-  if (questsTabBtn) questsTabBtn.click();
+  const tabBtn = document.querySelector('button[title=Quests]');
+  click(tabBtn);
+}
+
+// there might be a really clean XPath expression for this...
+function retweetAnyTweet() {
+  const actionQuests = [...document.querySelectorAll('.quest-action-container')];
+  const needsToRetweet = actionQuests.some(actionQuest => {
+    const isComplete = !actionQuest.querySelector('.quest-action-icon.icon-circle-empty');
+    if (isComplete) return false;
+    const questActionText = actionQuest.querySelector('.quest-action-text');
+    return questActionText.innerText && questActionText.innerText.toLowerCase().includes('retweet any tweet');
+  });
+  if (needsToRetweet) {
+    const retweetButton = document.querySelector('.icon-retweet');
+    click(retweetButton);
+  }
+}
+
+// consider using XPath here and analyzing text if these classes change somewhat frequently
+function vote() {
+  const voteButton = document.querySelector('.view__StyledButton-sc-1lnp3pd-2');
+  click(voteButton);
+  setTimeout(() => {
+    const option = document.querySelector('.styles__Answer-sc-8lpte4-2');
+    click(option);
+    if (option) {
+      const closePanel = document.querySelector('.SidebarActionButton-sc-856ady-0');
+      click(closePanel);
+    }
+  }, 1000);
 }
 
 const INTERVALS = Object.freeze({
   questsTab: 60 * 1000,
   claim: 5 * 1000,
   clickQuestActions: 5 * 60 * 1000, // every 5 mins because opening tabs frequently can be a problem if Gears' website has broken quests (as they usually do - it's TC...)
+  retweet: 5 * 60 * 1000,
+  vote: 15 * 1000,
   closeModal: 5 * 1000,
 });
 
 let questsInterval;
+let voteInterval;
+let retweetInterval;
 
 chrome.storage.sync.get(['autoClickQuests'], ({ autoClickQuests }) => {
   if (autoClickQuests) {
     questsInterval = setInterval(openQuestsTab, INTERVALS.questsTab);
+    retweetInterval = setInterval(retweetAnyTweet, INTERVALS.retweet);
+    voteInterval = setInterval(vote, INTERVALS.vote);
   }
 });
 
@@ -42,8 +81,12 @@ chrome.storage.onChanged.addListener((changes) => {
   if (changes.autoClickQuests) {
     if (changes.autoClickQuests.newValue) {
       questsInterval = setInterval(openQuestsTab, INTERVALS.questsTab);
+      retweetInterval = setInterval(retweetAnyTweet, INTERVALS.retweet);
+      voteInterval = setInterval(vote, INTERVALS.vote);
     } else {
       clearInterval(questsInterval);
+      clearInterval(voteInterval);
+      clearInterval(retweetInterval);
     }
   }
 });
